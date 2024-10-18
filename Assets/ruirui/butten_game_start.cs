471950735cfca853b996f69832b3ecadcb8814ac
ruirui <ruikeitai23@gmail.com>
2024-10-18 18:34:50 +0900

    タイトルシーンとボタン機能追加

---
+++ Assets/ruirui/butten_game_start.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; //ƒV[ƒ“Ø‚è‘Ö‚¦
+
+
+public class butten_game_start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(osu);   
+    }
+
+    // Update is called once per frame
+    void osu()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+}
