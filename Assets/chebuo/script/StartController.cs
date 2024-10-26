f4bd60323b70dead6d9d98da855083e8c4fdb107
chebuo <bob.chebuo@gmail.com>
2024-10-26 14:49:57 +0900

    title copy ,制限時間とカウントダウン

---
+++ Assets/chebuo/script/StartController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; //ÉVÅ[ÉìêÿÇËë÷Ç¶
+
+
+public class StartScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(osu);
+    }
+
+    // Update is called once per frame
+    void osu()
+    {
+        SceneManager.LoadScene("chebuo");
+
+    }
+}
